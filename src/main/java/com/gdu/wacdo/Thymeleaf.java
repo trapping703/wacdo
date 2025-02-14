4a331e66fbd932f0346d0464deb3a011d7d7105c
wacdoformation <wacdoformation@wacdo.fr>
2025-02-14 16:32:05 +0100

    fix de l'affichage des dates

---
+++ src/main/java/com/gdu/wacdo/Thymeleaf.java
@@ -0,0 +1,15 @@
+package com.gdu.wacdo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+
+@Configuration
+public class Thymeleaf {
+//
+//    @Bean
+//    public Java8TimeDialect java8TimeDialect() {
+//        return new Java8TimeDialect();
+//    }
+}
--- src/main/java/com/gdu/wacdo/WacdoConfiguration.java
+++ src/main/java/com/gdu/wacdo/WacdoConfiguration.java
@@ -3,6 +3,11 @@ package com.gdu.wacdo;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @Configuration
 public class WacdoConfiguration {
@@ -11,4 +16,5 @@ public class WacdoConfiguration {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
 }
