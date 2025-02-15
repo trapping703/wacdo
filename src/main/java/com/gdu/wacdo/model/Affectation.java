c0de0efab82db4651727992e79f80af5e2dc6213
wacdoformation <wacdoformation@wacdo.fr>
2025-02-15 15:23:29 +0100

    fetch lazy sur affectation

--- src/main/java/com/gdu/wacdo/model/Affectation.java
+++ src/main/java/com/gdu/wacdo/model/Affectation.java
@@ -1,6 +1,7 @@
 package com.gdu.wacdo.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
@@ -18,11 +19,11 @@ public class Affectation extends AbstractPersistentEntity<Integer> {
     @DateTimeFormat(pattern="yyyy-MM-dd")
     private LocalDate dateFin;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Employe employe;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Restaurant restaurant;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Fonction fonction;
 
     public Affectation(int id, LocalDate dateDebut, LocalDate dateFin, Employe employe, Restaurant restaurant, Fonction fonction) {
